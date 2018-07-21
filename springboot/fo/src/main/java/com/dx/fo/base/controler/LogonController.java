e1a1ef9e49c1e9fb2a8cb52b6a188599a351c448
duxingxing <820967275@qq.com>
2018-07-21 20:04:54 +0800

    提交

--- springboot/fo/src/main/java/com/dx/fo/base/controler/LogonController.java
+++ springboot/fo/src/main/java/com/dx/fo/base/controler/LogonController.java
@@ -11,15 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LogonController {
 
-    @RequestMapping("/index")
-    public String index() {
-        return "admin/index/index";
-    }
-    @RequestMapping("/welcome")
-    public String welcome() {
 
-        return "admin/index/welcome";
-    }
     @RequestMapping("/login")
     public String login() {
         return "ilogin";
